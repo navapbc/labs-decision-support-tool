0ee77d51f18515649554f9158dfbd70320fa7f9c
C Cheng <charliecheng@navapbc.com>
2024-06-25 16:19:31 -0400

    add generate function call to openai

--- app/src/chainlit.py
+++ app/src/chainlit.py
@@ -2,12 +2,15 @@ import logging
 
 import chainlit as cl
 
+from src.generate import generate
+
 logger = logging.getLogger(__name__)
 
 
 @cl.on_message
 async def main(message: cl.Message) -> None:
     logger.info(f"Received: {message.content}")
+    response = generate(message.content)
     await cl.Message(
-        content=f"Hello, world! Received: {message.content}",
+        content=response,
     ).send()
