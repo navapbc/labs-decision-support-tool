fbaa0e8a5aac4a4445786dfe79df571a72a3cdb5
C Cheng <charliecheng@navapbc.com>
2024-06-28 14:25:22 -0400

    fix format

--- app/tests/mock_completion.py
+++ app/tests/mock_completion.py
@@ -7,7 +7,4 @@ def mock_completion(model, messages):
     # Convert the messages list of dictionaries to a string
     messages = json.dumps(messages, sort_keys=True)
     mock_response = f"Called {model} with {messages}"
-    return completion(
-        model,
-        messages,
-        mock_response=mock_response)
+    return completion(model, messages, mock_response=mock_response)
--- app/tests/src/test_generate.py
+++ app/tests/src/test_generate.py
@@ -10,6 +10,6 @@ def test_get_models(monkeypatch):
 
 
 def test_generate(monkeypatch):
-    monkeypatch.setattr('src.generate.completion', mock_completion.mock_completion)
+    monkeypatch.setattr("src.generate.completion", mock_completion.mock_completion)
     expected_response = 'Called gpt-4o with [{"content": "Provide answers in plain language written at the average American reading level. \\n            Use bullet points. Keep your answers brief, max of 5 sentences. \\n            Keep your answers as similar to your knowledge text as you can", "role": "system"}, {"content": "some_string", "role": "user"}]'
     assert generate("some_string") == expected_response
