d742a0fd3bb58e0ccfe7a7c151f2bba3807b0d52
C Cheng <10414576+ccheng26@users.noreply.github.com>
2024-06-28 15:12:02 -0400

    DST-271 feat: generate llm result (#12)
    Added litellm call to prompt message and generate a response from gpt4o

--- app/src/generate.py
+++ app/src/generate.py
@@ -1,5 +1,7 @@
 import os
 
+from litellm import completion
+
 
 def get_models() -> dict[str, str]:
     """
@@ -12,3 +14,20 @@ def get_models() -> dict[str, str]:
     if "OPENAI_API_KEY" in os.environ:
         return {"OpenAI GPT-4o": "gpt-4o"}
     return {}
+
+
+def generate(query: str) -> str:
+    """
+    Returns a string response from an LLM model, based on a query input.
+    """
+    messages = [
+        {
+            "content": """Provide answers in plain language written at the average American reading level. 
+            Use bullet points. Keep your answers brief, max of 5 sentences. 
+            Keep your answers as similar to your knowledge text as you can""",
+            "role": "system",
+        },
+        {"content": query, "role": "user"},
+    ]
+    response = completion(model="gpt-4o", messages=messages)
+    return response["choices"][0]["message"]["content"]
