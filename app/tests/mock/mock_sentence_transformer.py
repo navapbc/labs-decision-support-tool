a9a734346ba39ff48d25b98684599cd6a7ebb386
Kevin Boyer <kevinboyer@navapbc.com>
2024-06-28 14:11:22 -0400

    Implement retrieve

--- app/tests/mock/mock_sentence_transformer.py
+++ app/tests/mock/mock_sentence_transformer.py
@@ -10,18 +10,18 @@ class MockSentenceTransformer:
     def encode(self, text, **kwargs):
         """
         Encode text into a 768-dimensional embedding that allows for similarity comparison via the dot product.
-        The embedding represents the average word length of the text
+        The direction of the vector maps to the average word length of the text.
         """
 
         tokens = self.tokenizer.tokenize(text)
         average_token_length = sum(len(token) for token in tokens) / len(tokens)
 
-        # Convert average word length to an angle, and pad the vector to length 768
-        angle = (1 / average_token_length) * 2 * math.pi
-        embedding = [math.cos(angle), math.sin(angle)] + ([0] * 766)
-
-        # Normalize the embedding before returning it
-        return [x / sum(embedding) for x in embedding]
+        # Map average length to between 0 and 90 degrees
+        # Then project that angle on the first two dimensions and pad the rest as 0
+        return [
+            math.cos(math.pi / 2 * average_token_length),
+            math.sin(math.pi / 2 * average_token_length),
+        ] + [0] * 766
 
 
 class MockTokenizer:
