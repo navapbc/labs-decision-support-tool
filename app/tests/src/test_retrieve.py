718763ccca747f245606a33010132cbe2c86ffa1
Kevin Boyer <kevinboyer@navapbc.com>
2024-07-01 12:08:26 -0400

    feat: Show accordions with cards (#16)
    * Implement accordions
    
    * Set theme to light mode
    
    * Update app/src/format.py
    
    Co-authored-by: Yoom Lam <yoom@navapbc.com>
    
    * Update app/src/retrieve.py
    
    Co-authored-by: Yoom Lam <yoom@navapbc.com>
    
    ---------
    
    Co-authored-by: Yoom Lam <yoom@navapbc.com>

--- app/tests/src/test_retrieve.py
+++ app/tests/src/test_retrieve.py
@@ -1,24 +1,39 @@
 from sqlalchemy import delete
 
 from src.db.models.document import Document
-from src.retrieve import retrieve
+from src.retrieve import retrieve, retrieve_with_scores
 from tests.mock.mock_sentence_transformer import MockSentenceTransformer
 from tests.src.db.models.factories import ChunkFactory
+
+
+def _get_chunks():
+    return [
+        ChunkFactory.create(
+            content="Incomprehensibility characterizes unintelligible, overwhelmingly convoluted dissertations."
+        ),
+        ChunkFactory.create(
+            content="Curiosity inspires creative, innovative communities worldwide."
+        ),
+        ChunkFactory.create(content="The quick brown red fox jumps."),
+    ]
 
 
 def test_retrieve(db_session, enable_factory_create):
     db_session.execute(delete(Document))
     mock_embedding_model = MockSentenceTransformer()
-
-    ChunkFactory.create(
-        content="Incomprehensibility characterizes unintelligible, overwhelmingly convoluted dissertations."
-    )
-    medium_chunk = ChunkFactory.create(
-        content="Curiosity inspires creative, innovative communities worldwide."
-    )
-    short_chunk = ChunkFactory.create(content="The quick brown red fox jumps.")
+    _, medium_chunk, short_chunk = _get_chunks()
 
     results = retrieve(db_session, mock_embedding_model, "Very tiny words.", k=2)
+
+    assert results == [short_chunk, medium_chunk]
+
+
+def test_retrieve_with_scores(db_session, enable_factory_create):
+    db_session.execute(delete(Document))
+    mock_embedding_model = MockSentenceTransformer()
+    _, medium_chunk, short_chunk = _get_chunks()
+
+    results = retrieve_with_scores(db_session, mock_embedding_model, "Very tiny words.", k=2)
 
     assert len(results) == 2
     assert results[0][0] == short_chunk
