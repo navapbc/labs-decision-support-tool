febc9b4d0e5518f48e5caed361bf27715acb335f
Kevin Boyer <kevinboyer@navapbc.com>
2024-07-02 13:13:45 -0400

    Add comment to require_login

--- app/src/login.py
+++ app/src/login.py
@@ -10,5 +10,9 @@ def login_callback(username: str, password: str) -> cl.User | None:
 
 
 def require_login() -> None:
+    # In addition to setting GLOBAL_PASSWORD, Chainlit also
+    # requires CHAINLIT_AUTH_SECRET to be set (to sign the
+    # authorization tokens.)
+
     if get_app_config().global_password:
         cl.password_auth_callback(login_callback)
