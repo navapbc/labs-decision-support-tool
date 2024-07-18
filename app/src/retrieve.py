195378e51b61c109024a1599f29fc8a657c200ef
Yoom Lam <yoom@navapbc.com>
2024-07-18 10:41:55 -0500

    feat: Retrieve from database based on filters (#29)
    Co-authored-by: Kevin Boyer <kevinboyer@navapbc.com>

--- app/src/retrieve.py
+++ app/src/retrieve.py
@@ -5,39 +5,48 @@ from sentence_transformers import SentenceTransformer
 from sqlalchemy import Row, select
 
 import src.adapters.db as db
-from src.db.models.document import Chunk
+from src.db.models.document import Chunk, Document
 
 logger = logging.getLogger(__name__)
 
 
 def retrieve(
-    db_session: db.Session, embedding_model: SentenceTransformer, query: str, k: int = 5
+    db_session: db.Session,
+    embedding_model: SentenceTransformer,
+    query: str,
+    k: int = 5,
+    **filters: Sequence[str] | None,
 ) -> Sequence[Chunk]:
-    logger.info(f"Retrieving context for {query!r}")
-
-    query_embedding = embedding_model.encode(query, show_progress_bar=False)
-
-    chunks = db_session.scalars(
-        select(Chunk).order_by(Chunk.mpnet_embedding.max_inner_product(query_embedding)).limit(k)
-    ).all()
-
-    for chunk in chunks:
-        logger.debug(f"Retrieved: {chunk.document.name!r}")
-
-    return chunks
+    chunks_with_scores = retrieve_with_scores(db_session, embedding_model, query, k, **filters)
+    return [chunk for chunk, _ in chunks_with_scores]
 
 
 def retrieve_with_scores(
-    db_session: db.Session, embedding_model: SentenceTransformer, query: str, k: int = 5
+    db_session: db.Session,
+    embedding_model: SentenceTransformer,
+    query: str,
+    k: int = 5,
+    **filters: Sequence[str] | None,
 ) -> Sequence[Row[Tuple[Chunk, float]]]:
-    logger.info(f"Retrieving context for {query!r}")
+    logger.info("Retrieving context for %r", query)
 
     query_embedding = embedding_model.encode(query, show_progress_bar=False)
 
+    statement = select(Chunk, Chunk.mpnet_embedding.max_inner_product(query_embedding)).join(
+        Chunk.document
+    )
+    if benefit_dataset := filters.pop("datasets", None):
+        statement = statement.where(Document.dataset.in_(benefit_dataset))
+    if benefit_programs := filters.pop("programs", None):
+        statement = statement.where(Document.program.in_(benefit_programs))
+    if benefit_regions := filters.pop("regions", None):
+        statement = statement.where(Document.region.in_(benefit_regions))
+
+    if filters:
+        raise ValueError(f"Unknown filters: {filters.keys()}")
+
     chunks_with_scores = db_session.execute(
-        select(Chunk, Chunk.mpnet_embedding.max_inner_product(query_embedding))
-        .order_by(Chunk.mpnet_embedding.max_inner_product(query_embedding))
-        .limit(k)
+        statement.order_by(Chunk.mpnet_embedding.max_inner_product(query_embedding)).limit(k)
     ).all()
 
     for chunk, score in chunks_with_scores:
--- app/tests/src/test_retrieve.py
+++ app/tests/src/test_retrieve.py
@@ -1,37 +1,93 @@
+import pytest
 from sqlalchemy import delete
 
 from src.db.models.document import Document
 from src.retrieve import retrieve, retrieve_with_scores
 from tests.mock.mock_sentence_transformer import MockSentenceTransformer
-from tests.src.db.models.factories import ChunkFactory
+from tests.src.db.models.factories import ChunkFactory, DocumentFactory
+
+mock_embedding_model = MockSentenceTransformer()
 
 
-def _get_chunks():
+def _create_chunks(document=None):
+    if not document:
+        document = DocumentFactory.create()
     return [
         ChunkFactory.create(
-            content="Incomprehensibility characterizes unintelligible, overwhelmingly convoluted dissertations."
+            document=document,
+            content="Incomprehensibility characterizes unintelligible, overwhelmingly convoluted dissertations.",
         ),
         ChunkFactory.create(
-            content="Curiosity inspires creative, innovative communities worldwide."
+            document=document,
+            content="Curiosity inspires creative, innovative communities worldwide.",
         ),
-        ChunkFactory.create(content="The quick brown red fox jumps."),
+        ChunkFactory.create(document=document, content="The quick brown red fox jumps."),
     ]
 
 
 def test_retrieve(db_session, enable_factory_create):
     db_session.execute(delete(Document))
-    mock_embedding_model = MockSentenceTransformer()
-    _, medium_chunk, short_chunk = _get_chunks()
+    _, medium_chunk, short_chunk = _create_chunks()
 
     results = retrieve(db_session, mock_embedding_model, "Very tiny words.", k=2)
 
     assert results == [short_chunk, medium_chunk]
 
 
+def test_retrieve__with_empty_filter(db_session, enable_factory_create):
+    db_session.execute(delete(Document))
+    _, medium_chunk, short_chunk = _create_chunks()
+
+    results = retrieve(db_session, mock_embedding_model, "Very tiny words.", k=2, datasets=[])
+
+    assert results == [short_chunk, medium_chunk]
+
+
+def test_retrieve__with_unknown_filter(db_session, enable_factory_create):
+    with pytest.raises(ValueError):
+        retrieve(
+            db_session, mock_embedding_model, "Very tiny words.", k=2, unknown_column=["some value"]
+        )
+
+
+def test_retrieve__with_dataset_filter(db_session, enable_factory_create):
+    db_session.execute(delete(Document))
+    _create_chunks(document=DocumentFactory.create())
+    _, snap_medium_chunk, snap_short_chunk = _create_chunks(
+        document=DocumentFactory.create(dataset="SNAP")
+    )
+
+    results = retrieve(
+        db_session,
+        mock_embedding_model,
+        "Very tiny words.",
+        k=2,
+        datasets=["SNAP"],
+    )
+    assert results == [snap_short_chunk, snap_medium_chunk]
+
+
+def test_retrieve__with_other_filters(db_session, enable_factory_create):
+    db_session.execute(delete(Document))
+    _create_chunks(document=DocumentFactory.create(program="Medicaid", region="PA"))
+    _, snap_medium_chunk, snap_short_chunk = _create_chunks(
+        document=DocumentFactory.create(program="SNAP", region="MI")
+    )
+
+    results = retrieve(
+        db_session,
+        mock_embedding_model,
+        "Very tiny words.",
+        k=2,
+        programs=["SNAP"],
+        regions=["MI"],
+    )
+    assert results == [snap_short_chunk, snap_medium_chunk]
+
+
 def test_retrieve_with_scores(db_session, enable_factory_create):
     db_session.execute(delete(Document))
-    mock_embedding_model = MockSentenceTransformer()
-    _, medium_chunk, short_chunk = _get_chunks()
+    _, medium_chunk, short_chunk = _create_chunks()
 
     results = retrieve_with_scores(db_session, mock_embedding_model, "Very tiny words.", k=2)
 
