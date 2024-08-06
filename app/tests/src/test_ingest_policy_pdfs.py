5045d158f86bd4087cd417f9aa1baeda59926586
C Cheng <charliecheng@navapbc.com>
2024-08-06 12:08:34 -0400

    directly use pdf file path for test

--- app/tests/src/test_ingest_policy_pdfs.py
+++ app/tests/src/test_ingest_policy_pdfs.py
@@ -9,11 +9,6 @@ from src.ingest_policy_pdfs import _ingest_policy_pdfs
 
 
 @pytest.fixture
-def policy_local_file():
-    return "/app/tests/docs/"
-
-
-@pytest.fixture
 def policy_s3_file(mock_s3_bucket_resource):
     data = open("/app/tests/docs/policy_pdf.pdf", "rb")
     mock_s3_bucket_resource.put_object(Body=data, Key="policy_pdf.pdf")
@@ -28,14 +23,12 @@ doc_attribs = {
 
 
 @pytest.mark.parametrize("file_location", ["local", "s3"])
-def test__ingest_policy_pdfs(
-    caplog, app_config, db_session, policy_s3_file, policy_local_file, file_location
-):
+def test__ingest_policy_pdfs(caplog, app_config, db_session, policy_s3_file, file_location):
     db_session.execute(delete(Document))
 
     with caplog.at_level(logging.INFO):
         if file_location == "local":
-            _ingest_policy_pdfs(policy_local_file, doc_attribs)
+            _ingest_policy_pdfs("/app/tests/docs/", doc_attribs)
         else:
             _ingest_policy_pdfs(policy_s3_file, doc_attribs)
 
