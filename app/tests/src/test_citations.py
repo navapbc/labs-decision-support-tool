42d1aac8e707a1f0db7e16d55e743cf15b4f060e
C Cheng <charliecheng@navapbc.com>
2024-08-23 17:09:11 -0400

    format to footnote

--- app/tests/src/test_citations.py
+++ app/tests/src/test_citations.py
@@ -3,10 +3,15 @@ from tests.src.db.models.factories import ChunkFactory
 
 
 def test_add_citations():
-    assert add_citations("This is a citation (chunk-0)", []) == "This is a citation (chunk-0)"
+    assert add_citations("This is a citation (chunk-0)", []) == "This is a citation (chunk-0)</br>"
 
     chunks = ChunkFactory.build_batch(2)
-    assert add_citations("This is a citation (chunk-0) and another (chunk-1).", chunks) == (
-        f"This is a citation ([{chunks[0].document.name}]({chunks[0].document}))"
-        f" and another ([{chunks[1].document.name}]({chunks[1].document}))."
+    assert all(
+        text in add_citations("This is a citation (chunk-0) and another (chunk-1).", chunks)
+        for text in [
+            "This is a citation",
+            chunks[0].document.name,
+            "and another",
+            chunks[1].document.name,
+        ]
     )
