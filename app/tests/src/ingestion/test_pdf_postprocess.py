903bab7acb82f7d54b2aac696dcedcdbb053bf8f
Yoom Lam <yoom@navapbc.com>
2024-08-28 16:01:50 -0500

    merges texts that are split across consecutive pages

--- app/tests/src/ingestion/test_pdf_postprocess.py
+++ app/tests/src/ingestion/test_pdf_postprocess.py
@@ -86,41 +86,55 @@ def test_concatenate_list_items():
             text="Introduction.",
             type=TextType.NARRATIVE_TEXT,
             headings=[Heading(title="Overview", level=1)],
+            page_number=1,
         ),
         EnrichedText(
             text="First item.",
             type=TextType.LIST_ITEM,
             headings=[Heading(title="Overview", level=1)],
+            page_number=1,
         ),
         EnrichedText(
             text="Second item.",
             type=TextType.LIST_ITEM,
             headings=[Heading(title="Overview", level=1)],
+            page_number=1,
         ),
         EnrichedText(
             text="Another narrative text.",
             type=TextType.NARRATIVE_TEXT,
             headings=[Heading(title="Overview", level=1)],
+            page_number=1,
         ),
         EnrichedText(
             text="Narrative starting a new list: ",  # ending space is intentional
             type=TextType.NARRATIVE_TEXT,
             headings=[Heading(title="Overview", level=1)],
+            page_number=1,
         ),
         EnrichedText(
             text="First item in new list.",
             type=TextType.LIST_ITEM,
             headings=[Heading(title="Overview", level=1)],
+            page_number=1,
         ),
         EnrichedText(
             text="Second item in new list.",
             type=TextType.LIST_ITEM,
             headings=[Heading(title="Overview", level=1)],
+            page_number=1,
         ),
         EnrichedText(
-            text="New list item in new section.",
+            text="New list item in new section",
             type=TextType.LIST_ITEM,
             headings=[Heading(title="Section 1", level=1)],
+            page_number=1,
+        ),
+        EnrichedText(
+            text="with continuing sentence on next page",
+            type=TextType.NARRATIVE_TEXT,
+            headings=[Heading(title="Section 1", level=1)],
+            page_number=2,
         ),
     ]
 
@@ -131,26 +145,31 @@ def test_concatenate_list_items():
             text="Introduction.",
             type=TextType.NARRATIVE_TEXT,
             headings=[Heading(title="Overview", level=1)],
+            page_number=1,
         ),
         EnrichedText(
             text="First item.\nSecond item.",
             type=TextType.LIST,
             headings=[Heading(title="Overview", level=1)],
+            page_number=1,
         ),
         EnrichedText(
             text="Another narrative text.",
             type=TextType.NARRATIVE_TEXT,
             headings=[Heading(title="Overview", level=1)],
+            page_number=1,
         ),
         EnrichedText(
             text="Narrative starting a new list: \nFirst item in new list.\nSecond item in new list.",
             type=TextType.LIST,
             headings=[Heading(title="Overview", level=1)],
+            page_number=1,
         ),
         EnrichedText(
-            text="New list item in new section.",
+            text="New list item in new section with continuing sentence on next page",
             type=TextType.LIST_ITEM,
             headings=[Heading(title="Section 1", level=1)],
+            page_number=1,
         ),
     ]
 
