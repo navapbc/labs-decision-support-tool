3f11ff9b28872eecd9cd601d16d47a7e61a8de7a
Yoom Lam <yoom@navapbc.com>
2024-11-15 11:55:22 -0600

    feat: Add tree-based chunking to EDD ingestion (#121)

--- app/tests/src/test_ingest_edd_web.py
+++ app/tests/src/test_ingest_edd_web.py
@@ -4,6 +4,7 @@ import logging
 import pytest
 from sqlalchemy import delete, select
 
+from src import ingest_edd_web
 from src.app_config import app_config as app_config_for_test
 from src.db.models.document import Document
 from src.ingest_edd_web import _ingest_edd_web
@@ -103,6 +104,7 @@ doc_attribs = {
 def test__ingest_edd(
     caplog, app_config, db_session, edd_web_local_file, edd_web_s3_file, file_location
 ):
+    ingest_edd_web.USE_MARKDOWN_TREE = False
     # Force a short max_seq_length to test chunking
     app_config_for_test.sentence_transformer.max_seq_length = 47
 
@@ -216,3 +218,89 @@ def test__ingest_edd(
             "The Northern Job Fairs and Workshops",
             "Scheduled Events",
         ]
+
+
+@pytest.mark.parametrize("file_location", ["local", "s3"])
+def test__ingest_edd_using_md_tree(
+    caplog, app_config, db_session, edd_web_local_file, edd_web_s3_file, file_location
+):
+    ingest_edd_web.USE_MARKDOWN_TREE = True
+    # Force a short max_seq_length to test chunking
+    app_config_for_test.sentence_transformer.max_seq_length = 47
+
+    db_session.execute(delete(Document))
+
+    with caplog.at_level(logging.WARNING):
+        if file_location == "local":
+            _ingest_edd_web(db_session, edd_web_local_file, doc_attribs)
+        else:
+            _ingest_edd_web(db_session, edd_web_s3_file, doc_attribs)
+
+    documents = db_session.execute(select(Document).order_by(Document.name)).scalars().all()
+    assert len(documents) == 4
+
+    assert (
+        "Skipping duplicate URL: https://edd.ca.gov/en/disability/options_to_file_for_di_benefits/"
+        in caplog.messages[0]
+    )
+
+    assert documents[0].name == "Nonindustrial Disability Insurance FAQs"
+    assert documents[0].source == "https://edd.ca.gov/en/disability/nonindustrial/faqs/"
+    assert documents[1].name == "Options to File for Disability Insurance Benefits"
+    assert (
+        documents[1].source == "https://edd.ca.gov/en/disability/options_to_file_for_di_benefits/"
+    )
+    assert documents[2].name == "State Unemployment Tax Act Dumping"
+    assert documents[2].source == "https://edd.ca.gov/en/payroll_taxes/suta_dumping/"
+    assert documents[3].name == "The Northern Job Fairs and Workshops"
+    assert documents[3].source == "https://edd.ca.gov/en/jobs_and_training/northern_region/"
+
+    # Document[0] has 3 sections, each section becomes its own chunk
+    doc0 = documents[0]
+    assert len(doc0.chunks) == 3
+    assert doc0.chunks[0].content.startswith(
+        "## Can I participate in either State Disability Insurance (SDI)"
+    )
+    assert doc0.chunks[1].content.startswith(
+        "## Will the State continue to contribute to my health, dental,"
+    )
+    assert doc0.chunks[2].content.startswith("## Nonindustrial Disability Insurance\n\n")
+    # All chunks have the same heading
+    for chunk in doc0.chunks:
+        assert chunk.headings == ["Nonindustrial Disability Insurance FAQs"]
+
+    # Document[1] is short
+    doc1 = documents[1]
+    assert len(doc1.chunks) == 1
+    assert doc1.chunks[0].content.startswith(
+        "Disability Insurance (DI) provides short-term, partial wage replacement ...\n\nIf you think you"
+    )
+    assert doc1.chunks[0].headings == ["Options to File for Disability Insurance Benefits"]
+
+    # Document[2] has a list
+    doc2 = documents[2]
+    assert len(doc2.chunks) == 4
+
+    # First section
+    assert doc2.chunks[0].headings == ["State Unemployment Tax Act Dumping"]
+    assert doc2.chunks[0].content.startswith(
+        "### SUTA Dumping Hurts Everyone\n\nEmployers, employees, and taxpayers"
+    )
+
+    # List is split into two chunks
+    assert (
+        doc2.chunks[1].headings
+        == doc2.chunks[2].headings
+        == [
+            "State Unemployment Tax Act Dumping",
+            "SUTA Dumping Hurts Everyone",
+        ]
+    )
+    assert doc2.chunks[1].content.startswith("SUTA dumping:\n\n* Costs the UI")
+    assert doc2.chunks[2].content.startswith(
+        "(SUTA dumping:)\n\n* Compromises the integrity of the UI system."
+    )
+
+    # Last section
+    assert doc2.chunks[3].headings == ["State Unemployment Tax Act Dumping"]
+    assert doc2.chunks[3].content.startswith("### [SUTA Dumping Schemes]")
