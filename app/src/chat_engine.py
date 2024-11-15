2904eb346614e307b6c9b476ba0e5ad19c653cab
Yoom Lam <yoom@navapbc.com>
2024-11-15 12:00:33 -0600

    revert retrieval_k

--- app/src/chat_engine.py
+++ app/src/chat_engine.py
@@ -171,7 +171,7 @@ class BridgesEligibilityManualEngine(BaseEngine):
 
 
 class CaEddWebEngine(BaseEngine):
-    retrieval_k: int = 5
+    retrieval_k: int = 50
     retrieval_k_min_score: float = -1
 
     # Note: currently not used
