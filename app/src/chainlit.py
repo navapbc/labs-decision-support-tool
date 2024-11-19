0107463de77db9b76c7e7b10ca3d0c45f60da708
Yoom Lam <yoom@navapbc.com>
2024-11-19 09:57:56 -0600

    refactor: Add ChatHistory class (#133)

--- app/src/chainlit.py
+++ app/src/chainlit.py
@@ -4,6 +4,8 @@ import pprint
 from typing import Any
 from urllib.parse import parse_qs, urlparse
 
+from asyncer import asyncify
+
 import chainlit as cl
 from chainlit.input_widget import InputWidget, Select, Slider, TextInput
 from chainlit.types import AskFileResponse
@@ -12,7 +14,7 @@ from src.app_config import app_config
 from src.batch_process import batch_process
 from src.chat_engine import ChatEngineInterface, OnMessageResult
 from src.format import build_accordions
-from src.generate import get_models
+from src.generate import ChatHistory, get_models
 from src.login import require_login
 
 logger = logging.getLogger(__name__)
@@ -151,8 +153,8 @@ _WIDGET_FACTORIES = {
 }
 
 
-def get_raw_chat_history(messages: list[cl.Message]) -> list[dict[str, str]]:
-    raw_chat_history: list[dict[str, str]] = []
+def get_raw_chat_history(messages: list[cl.Message]) -> ChatHistory:
+    raw_chat_history: ChatHistory = []
     for message in messages:
         if message.type == "assistant_message":
             raw_chat_history.append(
@@ -195,10 +197,8 @@ async def on_message(message: cl.Message) -> None:
         return
 
     try:
-        result = await cl.make_async(
-            lambda: engine.on_message(question=message.content, chat_history=chat_history)
-        )()
-        logger.info("Response: %s", result.response)
+        result = await asyncify(lambda: engine.on_message(message.content, chat_history))()
+        logger.info("Raw response: %s", result.response)
         if engine.formatter:
             # This block is to accommodate the old Guru chat engine
             msg_content = engine.formatter(
