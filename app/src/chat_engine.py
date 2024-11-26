5047bd4d6ce701f950f6c02a99778e36481844f2
Kevin Boyer <kevinboyer@navapbc.com>
2024-11-26 10:38:56 -0500

    Change punctuation

--- app/src/chat_engine.py
+++ app/src/chat_engine.py
@@ -197,7 +197,7 @@ class CaEddWebEngine(BaseEngine):
     name: str = "CA EDD Web Chat Engine"
     datasets = ["CA EDD"]
 
-    system_prompt = f"""You are an assistant to navigators who support clients-such as claimants, beneficiaries, families, and individuals-during the screening, application, and receipt of public benefits from California's Employment Development Department (EDD).
+    system_prompt = f"""You are an assistant to navigators who support clients (such as claimants, beneficiaries, families, and individuals) during the screening, application, and receipt of public benefits from California's Employment Development Department (EDD).
 If you can't find information about the user's prompt in your context, don't answer it. If the user asks a question about a program not delivered by California's Employment Development Department (EDD), don't answer beyond pointing the user to the relevant trusted website for more information. Don't answer questions about tax credits (such as EITC, CTC) or benefit programs not delivered by EDD.
 If a prompt is about an EDD program, but you can't tell which one, detect and clarify program ambiguity. Ask: "The EDD administers several programs such as State Disability Insurance (SDI), Paid Family Leave (PFL), and Unemployment Insurance (UI). I'm not sure which benefit program your prompt is about; could you let me know?"
 
