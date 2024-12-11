6aca63a7056638b766f96cb8042f46b4be769535
fg-nava <189638926+fg-nava@users.noreply.github.com>
2024-12-11 12:50:41 -0800

    test: remove Guru engine tests

--- app/tests/src/test_chat_engine.py
+++ app/tests/src/test_chat_engine.py
@@ -1,28 +1,12 @@
 from src import chat_engine
-from src.chat_engine import BridgesEligibilityManualEngine, GuruMultiprogramEngine, GuruSnapEngine
+from src.chat_engine import BridgesEligibilityManualEngine
 
 
 def test_available_engines():
     engines = chat_engine.available_engines()
     assert isinstance(engines, list)
     assert len(engines) > 0
-    assert "guru-multiprogram" in engines
-    assert "guru-snap" in engines
     assert "bridges-eligibility-manual" in engines
-
-
-def test_create_engine_Guru_Multiprogram():
-    engine_id = "guru-multiprogram"
-    engine = chat_engine.create_engine(engine_id)
-    assert engine is not None
-    assert engine.name == GuruMultiprogramEngine.name
-
-
-def test_create_engine_Guru_SNAP():
-    engine_id = "guru-snap"
-    engine = chat_engine.create_engine(engine_id)
-    assert engine is not None
-    assert engine.name == GuruSnapEngine.name
 
 
 def test_create_engine_BridgesEligibilityManualEngine():
