9db88009b594b6f7ccbca272bdc40235973917cf
fg-nava <189638926+fg-nava@users.noreply.github.com>
2025-02-11 15:46:06 -0800

    refactor: update postgres ssl logging to info level

--- app/tests/src/adapters/db/test_db.py
+++ app/tests/src/adapters/db/test_db.py
@@ -1,5 +1,6 @@
 import pytest
 from sqlalchemy import text
+import logging
 
 import src.adapters.db as db
 
@@ -11,6 +12,7 @@ def test_db_connection(db_client):
 
 
 def test_check_db_connection(caplog, monkeypatch: pytest.MonkeyPatch):
+    caplog.set_level(logging.INFO)
     monkeypatch.setenv("DB_CHECK_CONNECTION_ON_INIT", "True")
     db.PostgresDBClient()
     assert "database connection is not using SSL" in caplog.messages
