4b3ba50871b9fc3d8f21742a2ee2003d61347371
Kevin Boyer <kevinboyer@navapbc.com>
2025-02-20 13:08:44 -0500

    Format

--- infra/app/app-config/prod.tf
+++ infra/app/app-config/prod.tf
@@ -14,8 +14,8 @@ module "prod_config" {
   # These numbers are a starting point based on this article
   # Update the desired instance size and counts based on the project's specific needs
   # https://conchchow.medium.com/aws-ecs-fargate-compute-capacity-planning-a5025cb40bd0
-  service_cpu                    = 2048
-  service_memory                 = 8192
+  service_cpu    = 2048
+  service_memory = 8192
 
   # Enables ECS Exec access for debugging or jump access.
   # Defaults to `false`. Uncomment the next line to enable.
