d105f5469a8cddbbe9f8efbd13344253bff4f418
Yoom Lam <yoom@navapbc.com>
2025-02-28 09:42:12 -0600

    chore: Add echo_cmds() to refresh-ingestion.sh (#238)

--- app/refresh-ingestion.sh
+++ app/refresh-ingestion.sh
@@ -155,13 +155,34 @@ echo_stats(){
     echo "Markdown file count: $(find "${DATASET_ID}-${TODAY}_md" -type f -iname '*.md' | wc -l)"
     echo "-----------------------------------"
     echo "REMINDERS:"
-    echo "1. Upload the zip file to the 'Chatbot Knowledge Markdown' Google Drive folder, replacing the old zip file."
+    echo_cmds
+}
+
+echo_cmds(){
+    local DATASET_ID="$1"
+    echo "# 1. Upload the zip file to the 'Chatbot Knowledge Markdown' Google Drive folder, replacing the old zip file."
     echo "   $(ls -l "${DATASET_ID}_md.zip")"
-    echo "2. Upload ingester input files (e.g., *-scrapings.json) and stats to S3:"
-    echo "   aws s3 sync src/ingestion/${DATASET_ID}_scrapings*.json s3://decision-support-tool-app-dev/${DATASET_ID}"
-    echo "   aws s3 cp logs/${DATASET_ID}-${TODAY}_stats.json s3://decision-support-tool-app-dev/${DATASET_ID}/stats/${TODAY}_stats.json"
-    echo "3. Run ingestion on deployed app:"
-    echo "   ./bin/run-command app dev ..."
+    echo ""
+    echo "# 2. Upload ingester input files (e.g., *-scrapings.json) and stats to S3:"
+
+    if [ "$DATASET_ID" == "imagine_la" ]; then
+        local S3_HTML_DIR="s3://decision-support-tool-app-${DEPLOY_ENV}/imagine_la-${TODAY}"
+        echo "aws s3 sync app/src/ingestion/imagine_la/scrape/pages/ $S3_HTML_DIR/"
+        echo "aws s3 cp app/logs/${DATASET_ID}-${TODAY}_stats.json ${S3_HTML_DIR}/stats/${TODAY}_stats.json"
+        echo ""
+        echo "# 3. Run ingestion on deployed app:"
+        echo "./bin/run-command app ${DEPLOY_ENV} '[\"ingest-imagine-la\", \"Benefits Information Hub\", \"mixed\", \"California:LA County\", \"$S3_HTML_DIR\"]'"
+    elif [ "$DATASET_ID" == "ssa" ]; then
+        echo "The 'ssa' datasource was manually scraped, so it doesn't need to be refreshed."
+    else
+        local S3_DIR="s3://decision-support-tool-app-${DEPLOY_ENV}/${DATASET_ID}"
+        local S3_SCRAPINGS_FILE="${S3_DIR}/${DATASET_ID}_scrapings-${TODAY}.json"
+        echo "aws s3 cp app/src/ingestion/${DATASET_ID}_scrapings.json $S3_SCRAPINGS_FILE"
+        echo "aws s3 cp app/logs/${DATASET_ID}-${TODAY}_stats.json ${S3_DIR}/stats/${TODAY}_stats.json"
+        echo ""
+        echo "# 3. Run ingestion on deployed app:"
+        echo "./bin/run-command app $DEPLOY_ENV '[\"ingest-runner\", \"$DATASET_ID\", \"--json_input\", \"$S3_SCRAPINGS_FILE\"]'"
+    fi
 }
 
 check_preconditions(){
@@ -188,6 +209,7 @@ fi
 
 mkdir -p logs
 export TODAY=$(date "+%Y-%m-%d")
+: ${DEPLOY_ENV:=dev}
 
 case "$1" in
     imagine_la)
@@ -196,11 +218,18 @@ case "$1" in
     ssa)
         if ! [ -e "ssa_scrapings.json" ] || ! [ -d "ssa_extra_md" ]; then
             echo "ERROR: ssa_scrapings.json and ssa_extra_md/ folder are missing."
-            echo "Download them from https://us-east-1.console.aws.amazon.com/s3/buckets/decision-support-tool-app-dev?region=us-east-1&bucketType=general&prefix=ssa/"
+            echo "Download them from https://us-east-1.console.aws.amazon.com/s3/buckets/decision-support-tool-app-${DEPLOY_ENV}?region=us-east-1&bucketType=general&prefix=ssa/"
             exit 2
         fi
         EXTRA_INGEST_ARGS="--json_input=ssa_scrapings.json"
         scrape_and_ingest "$1"
+        ;;
+    cmds)
+        if [ -z "$2" ]; then
+            echo "Usage: '$0 cmds <DATASET_ID>'"
+            exit 3
+        fi
+        echo_cmds "$2"
         ;;
     *)
         scrape_and_ingest "$1"
