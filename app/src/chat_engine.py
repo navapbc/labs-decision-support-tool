88778a586102f42033de78b5e6e8251111e7d44d
Yoom Lam <yoom@navapbc.com>
2025-04-04 11:32:30 -0500

    reduce diffs

--- app/src/chat_engine.py
+++ app/src/chat_engine.py
@@ -358,12 +358,18 @@ Referral links: Format: [referral link title](referral link):
 - [Find Legal Aid](https://lafla.org/get-help/)
 - [See Federal Poverty Levels](https://aspe.hhs.gov/topics/poverty-economic-mobility/poverty-guidelines)
 
-If the user's question is related to any of the following policy updates listed below, set canned_response to empty string and set alert_message to one or more of the following text based on the user's question:
+If the user's question is related to any of the following policy updates listed below, \
+set canned_response to empty string and set alert_message to one or more of the following text based on the user's question:
 
 - Medi-Cal for immigrants: "Since January 1, 2024, everyone who lives in California can qualify for full-scope Medi-Cal, regardless of immigration status. All other Medi-Cal eligibility rules, including income limits, still apply. [Read more](https://www.coveredca.com/learning-center/information-for-immigrants/)."
 - Medi-Cal asset limits: "As of January 1, 2024, assets will no longer be counted to determine Medi-Cal eligibility. [Read more](https://www.dhcs.ca.gov/Get-Medi-Cal/Pages/asset-limits.aspx)"
-- CalFresh work requirements (ABAWDs, time limits): "California has a statewide waiver through October 31, 2025. This means no ABAWDs living in California will have to meet the work requirement to keep receiving CalFresh benefits. ABAWDs who have lost their CalFresh benefits may reapply and continue to receive CalFresh if otherwise eligible. [Read more](https://www.cdss.ca.gov/inforesources/calfresh/abawd)"
-- Calfresh asset limits/resource limits: "California has dramatically modified its rules for 'categorical eligibility' in the CalFresh program, such that asset limits have all but been removed. The only exceptions would be if either the household includes one or more members who are aged or disabled, with household income over 200% of the Federal Poverty Level (FPL); or the household fits within a narrow group of cases where it has been disqualified because of an intentional program violation, or some other specific compliance requirement; or there is a disputed claim for benefits paid in the past. [Read more](https://calfresh.guide/how-many-resources-a-household-can-have/#:~:text=In%20California%2C%20if%20the%20household,recipients%20have%20a%20resource%20limit)"
+- CalFresh work requirements (ABAWDs, time limits): "California has a statewide waiver through October 31, 2025. \
+This means no ABAWDs living in California will have to meet the work requirement to keep receiving CalFresh benefits. ABAWDs who have lost their CalFresh benefits may reapply and continue to receive CalFresh if otherwise eligible. [Read more](https://www.cdss.ca.gov/inforesources/calfresh/abawd)"
+- Calfresh asset limits/resource limits: "California has dramatically modified its rules for 'categorical eligibility' in the CalFresh program, \
+such that asset limits have all but been removed. The only exceptions would be if either the household includes one or more members who are aged or disabled, \
+with household income over 200% of the Federal Poverty Level (FPL); or the household fits within a narrow group of cases where it has been disqualified \
+because of an intentional program violation, or some other specific compliance requirement; or there is a disputed claim for benefits paid in the past. \
+[Read more](https://calfresh.guide/how-many-resources-a-household-can-have/#:~:text=In%20California%2C%20if%20the%20household,recipients%20have%20a%20resource%20limit)"
 
 If the user's question is to translate text, set needs_context to False.
 If the user's question is not in English, set translated_message to be an English translation of the user's message."""
