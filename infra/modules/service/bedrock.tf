79bbcddabf304c3c997a62aee208991e8b4d9f74
Yoom Lam <yoom@navapbc.com>
2025-04-07 16:51:12 -0500

    add bedrock.tf

---
+++ infra/modules/service/bedrock.tf
@@ -0,0 +1,27 @@
+#-----------------
+# Bedrock Access for using LiteLLM
+#-----------------
+resource "aws_iam_policy" "bedrock" {
+  name        = "${var.service_name}-bedrock-access"
+  description = "Allow access to AWS Bedrock from LiteLLM"
+  policy      = data.aws_iam_policy_document.bedrock.json
+}
+
+data "aws_iam_policy_document" "bedrock" {
+  # https://github.com/BerriAI/litellm/discussions/8949
+  statement {
+    sid    = "BedrockAccess"
+    effect = "Allow"
+    actions = [
+      "bedrock:InvokeModel",
+      "bedrock:InvokeModelWithResponseStream"
+    ]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "bedrock" {
+  count      = 1
+  role       = aws_iam_role.app_service.name
+  policy_arn = aws_iam_policy.bedrock.arn
+}
